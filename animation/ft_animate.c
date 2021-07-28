d4ffa13b5c68d26aedcb5b4c28a1a24a7652724d
Jonathan Fritz <fritz@nosc.io>
2021-07-28 09:07:52 +0200

    Animations now only occur in bonus

--- animation/ft_animate.c
+++ animation/ft_animate.c
@@ -6,7 +6,7 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/26 07:44:28 by jfritz            #+#    #+#             */
-/*   Updated: 2021/07/26 13:09:39 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/07/28 09:07:14 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,18 +37,21 @@ int	ft_animate(t_game_map **m, t_render_v **v)
 	static int			loop = 0;
 	static t_render_v	**vars;
 
-	if (v)
-		vars = v;
-	loop++;
-	if (loop == 0 && (vars && m))
-		state1(m, vars);
-	if (loop == 1 && (vars && m))
-		state2(m, vars);
-	if (loop == 2 && (vars && m))
-		state3(m, vars);
-	if (loop == 3 && (vars && m))
-		state4(m, vars);
-	if (loop == 4 && m && vars)
-		loop = 0;
+	if (BONUS)
+	{
+		if (v)
+			vars = v;
+		loop++;
+		if (loop == 0 && (vars && m))
+			state1(m, vars);
+		if (loop == 1 && (vars && m))
+			state2(m, vars);
+		if (loop == 2 && (vars && m))
+			state3(m, vars);
+		if (loop == 3 && (vars && m))
+			state4(m, vars);
+		if (loop == 4 && m && vars)
+			loop = 0;
+	}
 	return (1);
 }
